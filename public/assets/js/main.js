c5e63dc5116fc25b5bf72d7fe4180df40e36f8ea
MD Saifullah <muhammadsaifullah2223@gmail.com>
2025-05-04 19:11:28 +0530

    Fixed a bug related to VVS on small screens

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,17 +2,20 @@ document.addEventListener('click', function(event) {
     const target = event.target.closest('.element_container');
     const iframeContainer = document.getElementById('iframe_container');
     const iframe = document.getElementById('very_short_description');
+    const mainContainer = document.querySelector('.main_container'); // Reference to .main_container
 
-    if (iframe && iframeContainer) {
+    if (iframe && iframeContainer && mainContainer) {
         if (target && target.dataset.file) {
             iframe.src = 'very_short_descriptions/' + target.dataset.file;
             iframeContainer.style.display = 'block';  // Show the container
             iframe.style.display = 'block';  // Show the iframe
             iframeContainer.classList.add('VVS_appear');  // Add animation class
+            mainContainer.style.display = 'none';  // Hide the main container
         } else {
             iframeContainer.style.display = 'none';  // Hide the container
             iframe.style.display = 'none';  // Hide the iframe
             iframeContainer.classList.remove('VVS_appear');  // Remove animation class
+            mainContainer.style.display = 'block';  // Show the main container
         }
     }
 });
